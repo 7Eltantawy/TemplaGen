c34f17f947d7a8e060092a6b614531628062e14b
Hasan Ragab Eltantawy <50374022+HasanEltantawy@users.noreply.github.com>
2023-03-09 03:01:30 +0200

    Handle FolderTemplate Cases

--- src/actions/templagen-json-file/write-json-file.ts
+++ src/actions/templagen-json-file/write-json-file.ts
@@ -3,7 +3,12 @@ import {
   FolderTemplateSettingsData,
 } from "../../interfaces/template";
 import * as fs from "fs";
-import { folderTemplateSettingsFileName } from "../../utils";
+import {
+  folderTemplateSettingsFileName,
+  promptForBooleanCreateTemplateFolderDir,
+} from "../../utils";
+import { window } from "vscode";
+import { createDirectory } from "../dir-generator/create-dir";
 
 export async function writeToTemplaGenJson(template: FolderTemplate) {
   try {
@@ -12,17 +17,57 @@ export async function writeToTemplaGenJson(template: FolderTemplate) {
     const fileContents = await fs.readFileSync(filePath, "utf-8");
     let settings = JSON.parse(fileContents) as FolderTemplateSettingsData;
 
+    const flattenedSettingsDirs = flattenDirs(settings.dirs ?? {});
+    settings.dirs = flattenedSettingsDirs;
+    console.log(flattenedSettingsDirs);
+
+    const missedPaths: string[] = [];
+    for (const path of Object.keys(flattenedSettingsDirs)) {
+      if (!fs.existsSync(`${template.path}\\${path}`)) {
+        missedPaths.push(path);
+      }
+    }
+
+    let createMissied: boolean | undefined;
+    if (missedPaths.length > 0) {
+      createMissied = (
+        await promptForBooleanCreateTemplateFolderDir(missedPaths)
+      )?.data;
+    }
+
+    if (missedPaths.length > 0) {
+      // User Miss
+      if (createMissied === undefined) {
+        window.showWarningMessage(
+          "You choose to miss whether to create missied paths or not this can lead to unexpected behaviour"
+        );
+      }
+      // Create
+      else if (createMissied) {
+        for (const path of missedPaths) {
+          createDirectory(`${template.path}\\${path}`);
+        }
+      }
+      // Remove
+      else {
+        let filteredFlattenedSettingsDirs: Record<string, string[]> = {};
+        for (const path of Object.keys(flattenedSettingsDirs)) {
+          if (!missedPaths.includes(path)) {
+            filteredFlattenedSettingsDirs[path] = [];
+          }
+        }
+        settings.dirs = filteredFlattenedSettingsDirs;
+      }
+    }
+
     const paths = await getAllFolderPaths(template.path);
     const dirs: Record<string, string[]> = {};
     for (const folder of paths) {
       dirs[folder] = [];
     }
 
-    const result = Object.assign({}, dirs, flattenDirs(settings.dirs ?? {}));
-    settings.dirs = result;
-
+    settings.dirs = Object.assign({}, dirs, settings.dirs);
     const newFileContent = JSON.stringify(settings, null, 2);
-
     await fs.writeFileSync(filePath, newFileContent, "utf8");
   } catch (_) {
     console.log(_);
@@ -53,6 +98,8 @@ function flattenDirs(dirs: Record<string, string[]>): Record<string, string[]> {
   const flattenDirs: Record<string, string[]> = {};
 
   for (const item of Object.keys(dirs)) {
+    flattenDirs[item] = [];
+
     const list: string[] = dirs[item];
     for (const path of list) {
       flattenDirs[`${item}/${path}`] = [];
--- src/utils/prompts.ts
+++ src/utils/prompts.ts
@@ -11,31 +11,32 @@ import {
   FolderTemplate,
 } from "../interfaces/template";
 
-type QuickPickItemCustom = QuickPickItem & {
-  data: TemplateBase;
+type QuickPickItemCustom<T> = QuickPickItem & {
+  data: T;
 };
 
 export async function promptForSelectedTemplate(
   templates: Array<TemplateBase>
 ): Promise<TemplateBase | undefined> {
-  const data: QuickPickItemCustom | undefined = await promptForSelected(
-    templates
-  );
+  const data: QuickPickItemCustom<TemplateBase> | undefined =
+    await promptForSelected(templates);
 
   return data?.data;
 }
 
 function promptForSelected(
   templates: Array<TemplateBase>
-): Thenable<QuickPickItemCustom | undefined> {
-  const items: QuickPickItemCustom[] = templates.map((template) => {
-    const item: QuickPickItemCustom = {
-      label: template.name,
-      data: template,
-      detail: detailMaker(template),
-    };
-    return item;
-  });
+): Thenable<QuickPickItemCustom<TemplateBase> | undefined> {
+  const items: QuickPickItemCustom<TemplateBase>[] = templates.map(
+    (template) => {
+      const item: QuickPickItemCustom<TemplateBase> = {
+        label: template.name,
+        data: template,
+        detail: detailMaker(template),
+      };
+      return item;
+    }
+  );
 
   return window.showQuickPick(items, {
     placeHolder: "Select template",
@@ -85,5 +86,30 @@ export async function promptForTargetDirectory(): Promise<string | undefined> {
       return undefined;
     }
     return uri[0].fsPath;
+  });
+}
+
+export function promptForBooleanCreateTemplateFolderDir(
+  missedPath: string[]
+): Thenable<QuickPickItemCustom<boolean> | undefined> {
+  const items: QuickPickItemCustom<boolean>[] = [
+    {
+      label: "Create the missing folders",
+      data: true,
+      detail:
+        "All missied paths defined in templagen.json dirs will be created.",
+    },
+    {
+      label: "Remove missied paths from templagen.json",
+      data: false,
+      detail:
+        "All missing paths defined in templagen.json dirs will be removed.",
+    },
+  ];
+
+  return window.showQuickPick(items, {
+    title: "There are paths defined in templagen.json dirs which not exsit",
+    placeHolder: "Missied Paths: " + missedPath.join(" | "),
+    matchOnDescription: true,
   });
 }
